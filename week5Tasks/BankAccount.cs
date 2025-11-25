960c9c050a5d6bf2c1282b8c68f12873f40307fe
Kaushal55555 <lordqshal@gmail.com>
2025-11-25 11:04:52 +0545

    Add project files.

---
+++ week5Tasks/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿namespace OOPWorkshop
+{
+    internal class BankAccount
+    {
+        private string accountNumber;
+        private double balance;
+
+        public string AccountNumber
+        {
+            get { return accountNumber; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            private set
+            {
+                if (value > 0)
+                    balance = value;
+            }
+        }
+
+        public BankAccount(string accNo, double initialBalance)
+        {
+            accountNumber = accNo;
+            Balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+                Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= Balance)
+                Balance -= amount;
+        }
+    }
+}
---
+++ week5Tasks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOPWorkshop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== TASK 1: Encapsulation ===");
+            BankAccount acc = new BankAccount("ACC1001", 500);
+            Console.WriteLine("Account Number: " + acc.AccountNumber);
+            acc.Deposit(200);
+            acc.Withdraw(100);
+            Console.WriteLine("Final Balance: " + acc.Balance);
+        }
+    }
+}
